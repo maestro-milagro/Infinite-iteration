07de69a3df668091d5cd4d514d1f0efda6943c08
Sergey Korchagin <millagros77@gmail.com>
2023-04-02 17:16:10 +0300

    Functionality added

--- src/Randoms.java
+++ src/Randoms.java
@@ -1,2 +1,29 @@
-public class Randoms {
+import java.util.Iterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+    protected Random random = new Random();
+    protected int min;
+    protected int max;
+
+    public Randoms(int min, int max) {
+        this.max = max;
+        this.min = min;
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<>() {
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                return random.nextInt(min,max+1) ;
+            }
+        };
+    }
+
 }
